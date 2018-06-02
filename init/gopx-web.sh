0e586ac0f4309e29866e671249af9159b7f743cd
Rousan Ali <rousanali786@gmail.com>
2018-06-02 18:51:43 +0530

    Replace init folder with scripts folder

--- init/gopx-web.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-export $(cat init/.env | xargs) && go run cmd/gopx-web/*.go
--- init/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-go install ./cmd/gopx-web && export $(cat init/.env | xargs) && sudo gopx-web
